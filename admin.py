c6082fc751bebf5de635a1b7c240b5047e14d0cf
duonganhvu <duonganhvu555@gmail.com>
2026-06-28 15:02:07 +0700

    Add admin.py with QuestionInline, ChoiceInline, QuestionAdmin, LessonAdmin

---
+++ admin.py
@@ -0,0 +1,50 @@
+import datetime
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+from .models import Question, Choice, Submission, Lesson
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+    def was_published_recently(self, obj):
+        return obj.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
+class LessonAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Content', {'fields': ['content']}),
+        ('Timestamps', {'fields': ['created_at']}),
+    ]
+    list_display = ('title', 'created_at')
+    search_fields = ['title']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
+admin.site.register(Lesson, LessonAdmin)
--- models.py
+++ models.py
@@ -30,3 +30,12 @@ class Submission(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.question.question_text[:50]}"
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
