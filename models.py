ea325c09f0abc602dccb444ea946c94d58063422
duonganhvu <duonganhvu555@gmail.com>
2026-06-28 13:18:05 +0700

    Add Question, Choice, and Submission models

---
+++ models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Submission(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='submissions')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='submissions')
+    selected_choice = models.ForeignKey(Choice, on_delete=models.CASCADE, related_name='submissions')
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('question', 'user')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.question.question_text[:50]}"
